e5ac0fa5abf811d4eccfe9f19e715ba60fe7a926
unknown <rafaelaugusto.0112@gmail.com>
2025-11-22 00:02:21 -0300

    Adicionando blocos e final do mapa

--- minermath_alpha/contador.gd
+++ minermath_alpha/contador.gd
@@ -9,7 +9,7 @@ var multiplo_alvo: int = 0
 func _ready() -> void:
 	# Esta parte do código está perfeita e não precisa mudar.
 	randomize()
-	multiplo_alvo = randi_range(1, 8)
+	multiplo_alvo = randi_range(2, 9)
 	
 	# IMPORTANTE: Adicionamos uma verificação de segurança
 	if label_da_tela: # Checa se o label foi realmente conectado
