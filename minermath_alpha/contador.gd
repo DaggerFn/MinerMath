5a1b91ec2f44447c3ac8b578bc39b31a934b1765
Eduardo Hoeft <eduardofelipehoeft@gmail.com>
2025-12-04 12:27:25 -0300

    Adicionando lógica da pontuação

--- minermath_alpha/contador.gd
+++ minermath_alpha/contador.gd
@@ -1,20 +1,51 @@
 extends Node2D
 
-# Usamos @export para criar um campo no Inspetor. É mais seguro!
+# Variável exportada para o Label que mostra o múltiplo alvo
 @export var label_da_tela: Label
+
+# Usa @onready para referenciar seu nó "pontuacao" pelo caminho
+@onready var label_da_pontuacao: Label = $CanvasLayer/pontuacao 
+
 signal multiplo_alvo_pronto(numero_multiplo: int)
 
 var multiplo_alvo: int = 0
+var pontuacao: int = 0 # Variável para armazenar a pontuação atual
 
 func _ready() -> void:
-	# Esta parte do código está perfeita e não precisa mudar.
 	randomize()
 	multiplo_alvo = randi_range(2, 9)
 	
-	# IMPORTANTE: Adicionamos uma verificação de segurança
-	if label_da_tela: # Checa se o label foi realmente conectado
+	# 1. Configura e checa o Label do Múltiplo Alvo
+	if label_da_tela: 
 		label_da_tela.text = "Multiplo: " + str(multiplo_alvo)
 	else:
-		print("ERRO: O nó Label não foi conectado ao script no Inspetor!")
+		print("ERRO: O nó Label do alvo não foi conectado!")
+
+	# 2. Inicializa a exibição da pontuação
+	if label_da_pontuacao:
+		_atualizar_exibicao_pontuacao()
+	else:
+		print("ERRO: O nó Label da pontuação NÃO FOI ENCONTRADO no caminho: $CanvasLayer/pontuacao")
 		
 	multiplo_alvo_pronto.emit(multiplo_alvo)
+
+# --- Funções de Pontuação ---
+
+## Função chamada ao acertar um múltiplo
+func acertou_multiplo():
+	pontuacao += 1
+	_atualizar_exibicao_pontuacao()
+	print("Acerto! Pontuação: " + str(pontuacao))
+
+## Função chamada ao selecionar o número errado
+func errou_multiplo():
+	pontuacao -= 1
+	_atualizar_exibicao_pontuacao()
+	print("Erro! Pontuação: " + str(pontuacao))
+
+# --- Função de Exibição ---
+
+## Atualiza o texto do Label da pontuação na tela
+func _atualizar_exibicao_pontuacao():
+	if label_da_pontuacao:
+		label_da_pontuacao.text = "PONTOS: " + str(pontuacao)
